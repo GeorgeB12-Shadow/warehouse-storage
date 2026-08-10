72f1d49820e2fd9f7713d1342d299a1fa32f4809
GeorgeB12-Shadow <bout.phothilack@gmail.com>
2026-08-10 16:33:03 +0700

    Create Customer model with relationships and fillable fields

---
+++ app/Models/Customer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'company_name',
+        'contact_name',
+        'phone',
+        'email',
+        'tax_id',
+        'address',
+    ];
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
+}
