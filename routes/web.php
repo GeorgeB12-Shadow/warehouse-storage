17602d2138d0ecc13428f0a1bf846e68ad92d2fa
GeorgeB12-Shadow <bout.phothilack@gmail.com>
2026-08-13 23:07:53 +0700

    Add StorageLocationController routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\WarehouseController;
+use App\Http\Controllers\StorageLocationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,4 +13,7 @@ Route::resource('customers', CustomerController::class)
     ->only(['index', 'create', 'store', 'destroy']);
 
 Route::resource('warehouses', WarehouseController::class)
+    ->only(['index', 'create', 'store']);
+
+Route::resource('storage-locations', StorageLocationController::class)
     ->only(['index', 'create', 'store']);
